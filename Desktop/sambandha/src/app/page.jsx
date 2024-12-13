32eda48fdf693da65dfe12d5e4cac055dce3666c
jeetenmndl <jeetenmndl96@gmail.com>
2024-12-13 10:32:25 +0545

    all dependencies and packages installed

---
+++ Desktop/sambandha/src/app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Hello World 
+      <Button>Hello shadcn button</Button>
+    </div>
+  )
+}
+
+export default Home
